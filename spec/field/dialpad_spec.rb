decdf4b3e1c8f0852e776759bc2507657a717a01
artemave <artemave@gmail.com>
2010-06-03 21:15:15 +0000

    starts dialpad spec

---
+++ spec/field/dialpad_spec.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__).sub(/\/spec\/.*/, '') + '/spec/spec_helper.rb'
+
+require 'field/dialpad'
+
+describe Field::DialPad do
+  it 'should have 12 elements'
+
+  describe 'elements' do
+    it 'should contain digits, * and #'
+
+    it 'should be aligned like a phone dialpad'
+  end
+
+end
