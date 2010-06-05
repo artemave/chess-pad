8927ae5bdbc2b6d99d0fe2f7e8d3d159980ce1b7
artemave <artemave@gmail.com>
2010-06-05 15:50:00 +0000

    links element to container; introduces element neighbours; adds val to element

--- lib/field/element.rb
+++ lib/field/element.rb
@@ -1,9 +1,26 @@
 module Field
   class Element
-    attr_reader :x, :y
+    attr_reader :val, :x, :y
 
     def initialize(args)
-      @x, @y = args.values_at(:x, :y)
+      @val, @x, @y, @container = args.values_at(:val, :x, :y, :container)
+    end
+
+    def neighbours
+      neighbours = {}
+      neighbours[:n]  = @container[{:x => @x, :y => @y+1}]
+      neighbours[:ne] = @container[{:x => @x+1, :y => @y+1}]
+      neighbours[:e]  = @container[{:x => @x+1, :y => @y}]
+      neighbours[:se] = @container[{:x => @x+1, :y => @y-1}]
+      neighbours[:s]  = @container[{:x => @x, :y => @y-1}]
+      neighbours[:sw] = @container[{:x => @x-1, :y => @y-1}]
+      neighbours[:w]  = @container[{:x => @x-1, :y => @y}]
+      neighbours[:nw] = @container[{:x => @x-1, :y => @y+1}]
+      neighbours
+    end
+
+    def ==(value)
+      self.val == value.to_s
     end
   end
 end
